e5529078b6a4a16edd682df9947707281e5a873c
Thriller <wujie@lizhi.fm>
2020-02-04 00:46:32 +0800

    added: add platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TFlux",
+    platforms: [ .macOS(.v10_15) ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -24,5 +25,6 @@ let package = Package(
         .testTarget(
             name: "TFluxTests",
             dependencies: ["TFlux"]),
-    ]
+    ],
+    swiftLanguageVersions: [ .version("5.1") ]
 )
